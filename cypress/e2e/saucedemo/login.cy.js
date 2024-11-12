6da40a71a0eb56c8cf92de609a6b68369c931ab0
josefabioguimaraes <jfabioguimaraes@gmail.com>
2024-11-12 10:26:28 -0300

    first commit

---
+++ cypress/e2e/saucedemo/login.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+context('Testes da tela de Login', () => {
+
+    it('Login válido', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="password"]').type('secret_sauce');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="title"]').should('have.text','Products')
+                    // ou
+        cy.get('[data-test="title"]').should('contain','Product')
+
+        
+    });
+    
+});
